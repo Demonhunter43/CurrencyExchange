db8d91171103330429308174ce71376bf45cf70c
Vladislav <vladislav.vladvlad2015@yandex.ru>
2024-10-09 15:08:43 +0300

    Trying to migrate to another location on my PC

--- index.php
+++ index.php
@@ -1,2 +1,24 @@
 <?php
 require_once 'vendor/autoload.php';
+
+$hostname = 'localhost';
+$dbname = 'currencyexchange';
+$login = 'root';
+$password = '';
+$port = 3306;
+
+echo "loh";
+
+try {
+    $pdo = new PDO("mysql:host=$hostname;dbname=$dbname;port=$port;",$login,$password);
+} catch (PDOException $exception){
+    var_dump($exception->getMessage());
+}
+
+$sql = "SELECT * FROM `currencies`";
+
+$stmt = $pdo->query($sql);
+
+$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+var_dump($users);
